2d5c7eeda12795eb81816d1a88d8c55547c0723f
Liza <lizaamatya@gmail.com>
2022-08-18 10:48:53 +0545

    book management create and update

--- book_mgmt/books/urls.py
+++ book_mgmt/books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from books.views import BookListView, HomePageView
+from books.views import BookCreateView, BookListView, BookUpdateView, HomePageView
 
 app_name = "books"
 
 urlpatterns = [
     path('homepage/', HomePageView.as_view(), name="homepage"),
     path('', BookListView.as_view(), name="book_list"),
-    
+    path('create/', BookCreateView.as_view(), name="book_create"),
+    path('update/<int:pk>/', BookUpdateView.as_view(), name="book_update")
 ]
--- book_mgmt/books/views.py
+++ book_mgmt/books/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView, ListView
-from .models import Book
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView
+
+from books.forms import BookForm
+from books.models import Author, Book
 
 # Create your views here.
 # class HomePageView(View):
@@ -24,3 +27,35 @@ class BookListView(ListView):
         context.update({'description':"this is test descriptions"})
         
         return context
+    
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = "main_pages/book_create.html"
+    success_url = reverse_lazy('books:book_list')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context['authors'] = Author.objects.all()
+
+        return context
+    
+    def form_valid(self, form):
+        obj = form.save()
+        return super().form_valid(form)
+    
+    
+class BookUpdateView(UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = "main_pages/book_edit.html"
+    success_url = reverse_lazy('books:book_list')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context['authors'] = Author.objects.all()
+
+        return context
+    
