392683caf6db808be23f96d4d97b08ededa7173a
Liza <lizaamatya@gmail.com>
2022-08-22 10:46:27 +0545

    eg for signals

---
+++ book_mgmt/user_mgmt/signals.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# @receiver(post_save, sender=get_user_model())
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance,mobile_number=instance.phone_number)
+#     instance.profile.save()
